ec8581c817aa4f50aa364bbeab53fccec328f0e6
Tyreese2070 <tyreese.green1@outlook.com>
2026-03-18 21:27:56 +0000

    Added inverted indexer and save / load index methods

--- src/indexer.py
+++ src/indexer.py
@@ -1,12 +1,53 @@
 import json
 import string
+import os
+
+from crawler import crawl
 
 class Indexer:
     def __init__(self):
         self.index = {}
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.filepath = os.path.abspath(os.path.join(current_dir, "..", "data", "index.json"))
 
-    def build(self):
-        pass
+    def tokenise(self, text):
+        """
+        Convert text to lowercase, remove punctuation, and split into tokens
+        """
+        text = text.lower()
+        text = text.translate(str.maketrans("", "", string.punctuation))
+        tokens = text.split()
+        return tokens
+
+    def build(self, pages):
+        """
+        Build inverted index from parsed pages 
+        """
+        for page in pages:
+            url = page["url"]
+            text = page["text"]
+            words = self.tokenise(text)
+
+            for position, word in enumerate(words):
+                if word not in self.index:
+                    self.index[word] = {}
+                
+                if url not in self.index[word]:
+                    self.index[word][url] = {"frequency": 0, "positions": []}
+                
+                self.index[word][url]["frequency"] += 1
+                self.index[word][url]["positions"].append(position)
+    
+    def save(self):
+        os.makedirs(os.path.dirname(self.filepath), exist_ok=True)
+
+        with open(self.filepath, "w", encoding="utf-8") as f:
+            json.dump(self.index, f, ensure_ascii=False, indent=4)
 
     def load(self):
-        pass
+        """Load index from JSON file"""
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as f:
+                self.index = json.load(f)
+        except FileNotFoundError:
+            print("Index file not found.")
--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 
 def main():
     print("Enter a command")
-    command = input("> ").lower()
 
-    if command == "build":
-        print("building")
-    elif command == "load":
-        print("loading")
-    elif command == "print":
-        print("printing")
-    elif command == "find":
-        print("finding")
-    else:
-        print("Unknown command")
+    while True:
+        command = input("> ").lower()
+
+        if command == "build":
+            print("building")
+        elif command == "load":
+            print("loading")
+        elif command == "print":
+            print("printing")
+        elif command == "find":
+            print("finding")
+        elif command == "exit":
+            break
+        else:
+            print("Unknown command")
 
 if __name__ == "__main__":
     main()
