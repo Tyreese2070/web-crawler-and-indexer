e851ee12d324c7446e1849e07a327d6d068b896d
Tyreese2070 <tyreese.green1@outlook.com>
2026-03-18 15:02:09 +0000

    Added ininite loop test for crawl function

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -79,3 +79,35 @@ def test_crawl(mock_get_page_content, mock_sleep):
     assert result[1]["url"] == "https://test.com/page/2"
     assert result[1]["text"] == "Page 2"
     mock_sleep.assert_called_with(6)
+
+@patch("src.crawler.time.sleep")
+@patch("src.crawler.get_page_content")
+def test_crawl_infinite_loop(mock_get_page_content, mock_sleep):
+    """
+    Test that the crawl function doesn't get stuck in a loop
+    """
+
+    page_1 = """
+    <html>
+        <body>
+            <h1>Page 1</h1>
+            <li class="next"><a href="/page/2">Next</a></li>
+        </body>
+    </html>
+    """
+
+    page_2 = """
+    <html>
+        <body>
+            <h1>Page 2</h1>
+            <li class="next"> <a href="/page/1">Next</a></li>
+        </body>
+    </html>
+    """
+    mock_get_page_content.side_effect = [page_1, page_2, page_1, page_2]
+    result = crawl("https://test.com/page/1")
+
+    assert len(result) == 2
+    assert result[0]["url"] == "https://test.com/page/1"
+    assert result[1]["url"] == "https://test.com/page/2"
+
