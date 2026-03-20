d1dfee7935e671c70840a3ae45a9ad79b46c1c0a
Tyreese2070 <tyreese.green1@outlook.com>
2026-03-20 20:50:49 +0000

    updated command line interface with all working commands

--- src/main.py
+++ src/main.py
@@ -1,20 +1,69 @@
+from src.indexer import Indexer
+from src.crawler import crawl
+from src.search import print_word, find_query
 
 def main():
-    print("Enter a command")
+    print("Commands: build, load, print <word>, find <query>, exit")
+
+    search_engine = Indexer()
 
     while True:
-        command = input("> ").lower()
-
-        if command == "build":
-            print("building")
-        elif command == "load":
-            print("loading")
-        elif command == "print":
-            print("printing")
-        elif command == "find":
-            print("finding")
-        elif command == "exit":
+        try:
+            user_input = input("> ").strip()
+        except (EOFError, KeyboardInterrupt):
+            print("\nExiting")
             break
+
+        if not user_input:
+            continue
+
+        if user_input.lower() in ["exit", "quit"]:
+            print("Exiting")
+            break
+
+        command_parts = user_input.split(maxsplit=1)
+        command = command_parts[0].lower()
+
+        # Build command
+        if command == "build":
+            print("Crawling and building index")
+            crawled_data = crawl("https://quotes.toscrape.com/")
+
+            if crawled_data:
+                print("Building index")
+                search_engine.index = {}
+                search_engine.build(crawled_data)
+                search_engine.save()
+                print("Built and saved index")
+            else:
+                print("Failed to crawl")
+
+        # Load command
+        elif command == "load":
+            search_engine.load()
+            if search_engine.index:
+                print("Index loaded")
+            else:
+                print("Failed to load index, run build first")
+
+        # Print command
+        elif command == "print":
+            if len(command_parts) < 2:
+                print("Usage: print <word>")
+            elif not search_engine.index:
+                print("Index not loaded, run build or load first")
+            else:
+                print_word(search_engine, command_parts[1])
+        
+        # Find command
+        elif command == "find":
+            if len(command_parts) < 2:
+                print("Usage: find <query>")
+            elif not search_engine.index:
+                print("Index not loaded, run build or load first")
+            else:
+                find_query(search_engine, command_parts[1])
+        
         else:
             print("Unknown command")
 
