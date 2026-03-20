75f7e7d13264f87f9a98b8a1b0adb2bf589bb373
Tyreese2070 <tyreese.green1@outlook.com>
2026-03-20 20:05:46 +0000

    Added find query function for search engine

--- src/search.py
+++ src/search.py
@@ -15,12 +15,26 @@ def print_word(indexer, word):
         print(f"{word} not found in index")
 
 def find_query(indexer, query):
-    pass
+    tokens = indexer.tokenise(query)
 
-def main():
-    pass
+    if not tokens:
+        print("Invalid query")
+        return
+    
+    if tokens[0] not in indexer.index:
+        print("No pages found")
+        return
+    
+    matching_urls = set(indexer.index[tokens[0]].keys())
 
-if __name__ == "__main__":
-    test_indexer = Indexer()
-    test_indexer.load()
-    print_word(test_indexer, "chicken")
+    for token in tokens[1:]:
+        if token not in indexer.index:
+            matching_urls = set()
+            break
+        matching_urls.intersection_update(set(indexer.index[token].keys()))
+
+    if not matching_urls:
+        print("No pages found")
+    else:
+        for url in matching_urls:
+            print(url)
